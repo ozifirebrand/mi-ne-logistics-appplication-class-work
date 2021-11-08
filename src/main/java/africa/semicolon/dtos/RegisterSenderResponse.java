03814caf6e54b95a7a9961f9194049bfebcbd26a
Ozioma <oziomaokoroafor@gmail.com>
2021-11-08 16:49:24 +0100

    Added dtos

--- src/main/java/africa/semicolon/dtos/RegisterSenderResponse.java
+++ src/main/java/africa/semicolon/dtos/RegisterSenderResponse.java
@@ -1,5 +1,8 @@
 package africa.semicolon.dtos;
 
+import lombok.Data;
+
+@Data
 public class RegisterSenderResponse {
     private String senderEmail;
 }
--- src/main/java/africa/semicolon/services/SenderServiceImpl.java
+++ src/main/java/africa/semicolon/services/SenderServiceImpl.java
@@ -1,11 +1,24 @@
 package africa.semicolon.services;
 
+import africa.semicolon.data.models.Sender;
+import africa.semicolon.data.repositories.SenderRepository;
+import africa.semicolon.data.repositories.SenderRepositoryImpl;
 import africa.semicolon.dtos.RegisterSenderResponse;
 import africa.semicolon.dtos.Requests.RegisterSenderRequest;
 
 public class SenderServiceImpl implements SenderService{
+    private static final SenderRepository senderRepository = new SenderRepositoryImpl();
     @Override
     public RegisterSenderResponse registerSender(RegisterSenderRequest registerSenderRequest) {
-        return null;
+        Sender sender = new Sender();
+        sender.setSenderName(registerSenderRequest.getSenderName());
+        sender.setPhoneNumber(registerSenderRequest.getPhoneNumber());
+        sender.setEmailAddress(registerSenderRequest.getSenderEmail());
+
+        Sender savedSender = senderRepository.save(sender);
+
+        RegisterSenderResponse response = new RegisterSenderResponse();
+        response.setSenderEmail(savedSender.getEmailAddress());
+        return response;
     }
 }
